44811650f5d28fae04e1aeae1f4419b476e215d0
Tony Lam <tony.lam@iki.fi>
2025-02-17 11:08:25 +0200

    feat: frontend fetches data from backend

--- frontend/src/components/dayChart.tsx
+++ frontend/src/components/dayChart.tsx
@@ -1,8 +1,21 @@
+import { useState, useEffect } from 'react';
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
 import { get_stock_data } from '../utils/parser';
 
 export const DayChart = () => {
+  const [chartData, setChartData] = useState<[number, number][]>([]);
+
+  // use effect for waiting the stock data to load
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await get_stock_data();
+      setChartData(data);
+    };
+
+    fetchData();
+  }, []);
+
   const options = {
     title: {
       text: 'Apple Inc. stock price',
@@ -11,7 +24,7 @@ export const DayChart = () => {
       {
         type: 'line',
         name: 'AAPL',
-        data: get_stock_data(),
+        data: chartData,
         tooltip: {
           valueDecimals: 2,
         },
