59cba1c4cb13604ed8dd9c18969f233c3aacdc84
Tony Lam <tony.lam@iki.fi>
2025-02-17 11:07:11 +0200

    feat: enable CORS in backend

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { get_daily_stock } from './utils/stock_api';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
